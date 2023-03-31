a0c249a85b1b382aa551a23b9daa41a6ffcf2d02
paolo berlanda <97700927+pberlanda@users.noreply.github.com>
2023-03-31 08:35:16 +0200

    app completa

---
+++ copyTemi/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnChiudi = New System.Windows.Forms.Button()
+        Me.btnOk = New System.Windows.Forms.Button()
+        Me.LabelTemi = New System.Windows.Forms.Label()
+        Me.NumericUpDownTemiSvoltiInizio = New System.Windows.Forms.NumericUpDown()
+        Me.LabelDestinazione = New System.Windows.Forms.Label()
+        Me.TextBoxDestinazione = New System.Windows.Forms.TextBox()
+        Me.ProgressBar = New System.Windows.Forms.ProgressBar()
+        Me.btnSelezionaCartella = New System.Windows.Forms.Button()
+        Me.NumericUpDownTemiSvoltiFine = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDownTemiSvoltiInizio, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDownTemiSvoltiFine, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnChiudi
+        '
+        Me.btnChiudi.Location = New System.Drawing.Point(397, 97)
+        Me.btnChiudi.Name = "btnChiudi"
+        Me.btnChiudi.Size = New System.Drawing.Size(75, 23)
+        Me.btnChiudi.TabIndex = 4
+        Me.btnChiudi.Text = "Chiudi"
+        Me.btnChiudi.UseVisualStyleBackColor = True
+        '
+        'btnOk
+        '
+        Me.btnOk.Location = New System.Drawing.Point(316, 97)
+        Me.btnOk.Name = "btnOk"
+        Me.btnOk.Size = New System.Drawing.Size(75, 23)
+        Me.btnOk.TabIndex = 3
+        Me.btnOk.Text = "Ok"
+        Me.btnOk.UseVisualStyleBackColor = True
+        '
+        'LabelTemi
+        '
+        Me.LabelTemi.AutoSize = True
+        Me.LabelTemi.Location = New System.Drawing.Point(14, 24)
+        Me.LabelTemi.Name = "LabelTemi"
+        Me.LabelTemi.Size = New System.Drawing.Size(110, 13)
+        Me.LabelTemi.TabIndex = 2
+        Me.LabelTemi.Text = "Copia temi (da 1 a 51)"
+        '
+        'NumericUpDownTemiSvoltiInizio
+        '
+        Me.NumericUpDownTemiSvoltiInizio.Location = New System.Drawing.Point(134, 22)
+        Me.NumericUpDownTemiSvoltiInizio.Name = "NumericUpDownTemiSvoltiInizio"
+        Me.NumericUpDownTemiSvoltiInizio.Size = New System.Drawing.Size(45, 20)
+        Me.NumericUpDownTemiSvoltiInizio.TabIndex = 0
+        '
+        'LabelDestinazione
+        '
+        Me.LabelDestinazione.AutoSize = True
+        Me.LabelDestinazione.Location = New System.Drawing.Point(12, 57)
+        Me.LabelDestinazione.Name = "LabelDestinazione"
+        Me.LabelDestinazione.Size = New System.Drawing.Size(103, 13)
+        Me.LabelDestinazione.TabIndex = 4
+        Me.LabelDestinazione.Text = "Dove salvaere i temi"
+        '
+        'TextBoxDestinazione
+        '
+        Me.TextBoxDestinazione.Location = New System.Drawing.Point(134, 54)
+        Me.TextBoxDestinazione.Name = "TextBoxDestinazione"
+        Me.TextBoxDestinazione.Size = New System.Drawing.Size(308, 20)
+        Me.TextBoxDestinazione.TabIndex = 1
+        Me.TextBoxDestinazione.Text = "O:\"
+        '
+        'ProgressBar
+        '
+        Me.ProgressBar.Location = New System.Drawing.Point(15, 97)
+        Me.ProgressBar.Name = "ProgressBar"
+        Me.ProgressBar.Size = New System.Drawing.Size(287, 23)
+        Me.ProgressBar.TabIndex = 5
+        '
+        'btnSelezionaCartella
+        '
+        Me.btnSelezionaCartella.Location = New System.Drawing.Point(448, 52)
+        Me.btnSelezionaCartella.Name = "btnSelezionaCartella"
+        Me.btnSelezionaCartella.Size = New System.Drawing.Size(24, 23)
+        Me.btnSelezionaCartella.TabIndex = 6
+        Me.btnSelezionaCartella.Text = "..."
+        Me.btnSelezionaCartella.UseVisualStyleBackColor = True
+        '
+        'NumericUpDownTemiSvoltiFine
+        '
+        Me.NumericUpDownTemiSvoltiFine.Location = New System.Drawing.Point(186, 22)
+        Me.NumericUpDownTemiSvoltiFine.Name = "NumericUpDownTemiSvoltiFine"
+        Me.NumericUpDownTemiSvoltiFine.Size = New System.Drawing.Size(40, 20)
+        Me.NumericUpDownTemiSvoltiFine.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(487, 136)
+        Me.Controls.Add(Me.NumericUpDownTemiSvoltiFine)
+        Me.Controls.Add(Me.btnSelezionaCartella)
+        Me.Controls.Add(Me.ProgressBar)
+        Me.Controls.Add(Me.TextBoxDestinazione)
+        Me.Controls.Add(Me.LabelDestinazione)
+        Me.Controls.Add(Me.NumericUpDownTemiSvoltiInizio)
+        Me.Controls.Add(Me.LabelTemi)
+        Me.Controls.Add(Me.btnOk)
+        Me.Controls.Add(Me.btnChiudi)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Copy temi"
+        CType(Me.NumericUpDownTemiSvoltiInizio, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDownTemiSvoltiFine, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnChiudi As System.Windows.Forms.Button
+    Friend WithEvents btnOk As System.Windows.Forms.Button
+    Friend WithEvents LabelTemi As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDownTemiSvoltiInizio As System.Windows.Forms.NumericUpDown
+    Friend WithEvents LabelDestinazione As System.Windows.Forms.Label
+    Friend WithEvents TextBoxDestinazione As System.Windows.Forms.TextBox
+    Friend WithEvents ProgressBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents btnSelezionaCartella As System.Windows.Forms.Button
+    Friend WithEvents NumericUpDownTemiSvoltiFine As System.Windows.Forms.NumericUpDown
+
+End Class
---
+++ copyTemi/Form1.vb
@@ -0,0 +1,163 @@
+﻿Public Class Form1
+
+    Private Sub btnChiudi_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnChiudi.Click
+
+        ' chiude il programma
+
+        End
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        ' nascondi la progress bar
+
+        ProgressBar.Visible = False
+
+        ' numero minimo e massimo di temi
+
+        NumericUpDownTemiSvoltiInizio.Minimum = 1
+        NumericUpDownTemiSvoltiInizio.Maximum = 51
+
+        NumericUpDownTemiSvoltiFine.Minimum = 1
+        NumericUpDownTemiSvoltiFine.Maximum = 51
+
+        TextBoxDestinazione.Text = "O:\temi\"
+
+        ' focus sul primo controllo
+
+        NumericUpDownTemiSvoltiInizio.Focus()
+
+        ' Ok come degault
+
+        Me.AcceptButton = btnOk
+
+    End Sub
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+        Dim k As Decimal
+
+        ' controllo
+
+        If controllo() = False Then Exit Sub
+
+        ' Chiede conferma
+
+        If MsgBox("Sei sicuro di voler copiare tutti i temi nella cartella " + TextBoxDestinazione.Text + "?", vbYesNo) = vbNo Then Exit Sub
+
+        ' progress bar
+
+        ProgressBar.Minimum = NumericUpDownTemiSvoltiInizio.Value
+        ProgressBar.Maximum = NumericUpDownTemiSvoltiFine.Value
+
+        ProgressBar.Visible = True
+
+        ' copia i temi richiesti
+
+        For k = NumericUpDownTemiSvoltiInizio.Value To NumericUpDownTemiSvoltiFine.Value
+
+            ' aggiorna la progress bar
+
+            ProgressBar.Increment(CInt(k))
+
+            ' copia il tema
+
+            If copiaTema(k) = False Then
+                MsgBox("Errore!")
+                Exit Sub
+            End If
+
+        Next
+
+        ' Nasconde la progress bar
+
+        ProgressBar.Hide()
+
+        ' Ok
+
+        MsgBox("Copia dei temi completata!", vbInformation)
+
+    End Sub
+
+    Private Function copiaTema(ByVal numeroTema As Decimal) As Boolean
+        Dim numeroTemaStringa As String
+        Dim sourcePath As String
+        Dim XCopyString As String
+
+        ' copia fallita
+
+        copiaTema = False
+
+        ' converti in stringa il numero tema
+
+        numeroTemaStringa = convertiNumeroTema(numeroTema)
+
+        ' compone la sringa del percorso di origine del tema
+
+        sourcePath = componePercorsoOrigine(numeroTemaStringa)
+
+        ' copia il tema con XCOPY
+
+        XCopyString = "cmd.exe /C xcopy.exe " + sourcePath + "\*.* " + TextBoxDestinazione.Text + "tema" + numeroTemaStringa + " /E /I"
+
+        Shell(XCopyString, AppWinStyle.Hide)
+
+        ' Ok
+
+        copiaTema = True
+
+    End Function
+
+    Private Function convertiNumeroTema(ByVal numeroTema As Decimal) As String
+
+        ' Aggiunge 0 davanti al numero se minore di 10
+
+        If numeroTema < 10 Then
+            convertiNumeroTema = "0" + CStr(numeroTema)
+        Else
+            convertiNumeroTema = CStr(numeroTema)
+        End If
+
+    End Function
+
+    Private Function componePercorsoOrigine(ByVal numeroTema As String) As String
+
+        componePercorsoOrigine = "\\arlecchino\tema" + numeroTema + "$"
+
+    End Function
+
+    Private Sub btnSelezionaCartella_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSelezionaCartella.Click
+        Dim folder As New FolderBrowserDialog
+
+        If folder.ShowDialog = Windows.Forms.DialogResult.OK Then
+            TextBoxDestinazione.Text = folder.SelectedPath
+        End If
+
+    End Sub
+
+    Private Function controllo() As Boolean
+
+        ' fallito
+
+        controllo = False
+
+        ' tema fine non può essere minore di tema inizio
+
+        If NumericUpDownTemiSvoltiFine.Value < NumericUpDownTemiSvoltiInizio.Value Then
+            MsgBox("Il numero dell'ultimo tema non può essere minore del primo!", vbCritical)
+            NumericUpDownTemiSvoltiFine.Focus()
+            controllo = False
+            Exit Function
+        End If
+
+        ' controlla l'ultimo carattere. se manca \ la aggiunge
+
+        If GetChar(TextBoxDestinazione.Text, Len(TextBoxDestinazione.Text)) <> "\" Then TextBoxDestinazione.Text = TextBoxDestinazione.Text + "\"
+
+        ' ok
+
+        controllo = True
+
+    End Function
+
+End Class
